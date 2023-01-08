f53718d9ad2b36ef672dba742d4c7c3647072871
Pierre Allix <pierre.allix.work@gmail.com>
2023-01-08 16:04:02 +0100

    Add tapa enumerables skeleton

---
+++ tapa_enumerables/lib/tapa_enumerables.ex
@@ -0,0 +1,18 @@
+defmodule TapaEnumerables do
+  @moduledoc """
+  Documentation for `TapaEnumerables`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> TapaEnumerables.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
